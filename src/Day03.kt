c94c053349f8506d5994a5d81c03cf3129ad0621
codedchai <guardianx515@gmail.com>
2024-12-03 11:58:37 -0600

    Day 3 part 2

--- src/Day03.kt
+++ src/Day03.kt
@@ -16,5 +16,33 @@ fun main() {
     }
   }
 
+  fun part2(): Int {
+    val input = readAllInput()
+    val mulIdentificationRegex = Regex("""mul\(\d{1,3},\d{1,3}\)""")
+    val enableRegex = Regex("""do\(\)""")
+    val disableRegex = Regex("""don't\(\)""")
+    val digitRegex = Regex("""\d{1,3}""")
+
+    var enabled = true
+    var total = 0
+    for (i in input.indices) {
+      if (enabled) {
+        mulIdentificationRegex.matchAt(input, i)?.also {
+          val mulResult = digitRegex.findAll(it.value)
+            .fold(1) { total, matchResult ->
+              total * matchResult.value.toInt()
+            }.toInt()
+          total += mulResult
+        }
+        disableRegex.matchAt(input, i)?.also { enabled = false }
+      } else {
+        enableRegex.matchAt(input, i)?.also { enabled = true }
+      }
+    }
+
+    return total
+  }
+
   println("Part 1: ${part1()}")
+  println("Part 2: ${part2()}")
 }
