70fd7700fa283e37d60153cf26df7d1662a00164
codedchai <guardianx515@gmail.com>
2024-12-10 10:29:29 -0600

    Day 10 part 2

--- src/Day10.kt
+++ src/Day10.kt
@@ -24,5 +24,29 @@ fun main() {
     return endpointsWeCanHikeTo.size
   }
 
+  fun traversePart2(grid: Grid<Int>, currentPosition: Vec2, currentHeight: Int): List<Vec2> {
+    val currentGridValue = grid[currentPosition]
+    if (currentGridValue != currentHeight) {
+      return emptyList()
+    } else if (currentHeight == 9) {
+      return listOf(currentPosition)
+    }
+    return fourDirections.flatMap { traversePart2(grid, currentPosition.plus(it), currentHeight + 1) }
+  }
+
+  fun part2(): Int {
+    val grid = readGridInput("Day10").coordinatesToValues.entries.associateTo(hashMapOf()) { (key, value) ->
+      key to value.toString().toInt()
+    }.let { Grid(it) }
+    val endpointsWeCanHikeTo = grid.yIndices.flatMap { y ->
+      grid.xIndices.flatMap { x ->
+        traversePart2(grid, Vec2(x, y), 0)
+      }
+    }
+
+    return endpointsWeCanHikeTo.size
+  }
+
   part1().println()
+  part2().println()
 }
