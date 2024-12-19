b1a190a130a2cac3bc745486a6aaca853c130230
codedchai <guardianx515@gmail.com>
2024-12-19 16:31:52 -0600

    Day 19 part 2, big numbers require Longs

--- src/Day19.kt
+++ src/Day19.kt
@@ -13,15 +13,16 @@ fun main() {
     return Linen(availablePatterns, desiredDesigns)
   }
 
-  val cache = hashMapOf<Pair<String, List<String>>, Boolean>()
-  fun isValidDesign(desiredDesign: String, availablePatterns: List<String>): Boolean {
-    desiredDesign.ifEmpty { return true }
-    return cache.getOrPut(Pair(desiredDesign, availablePatterns)) {
+  val cache = hashMapOf<String, Long>()
+  fun countValidDesigns(desiredDesign: String, availablePatterns: List<String>): Long {
+    if (desiredDesign.isEmpty()) {
+      return 1
+    }
+    return cache.getOrPut(desiredDesign) {
       val validPatterns = availablePatterns.filter { desiredDesign.startsWith(it) }
-      validPatterns.ifEmpty { return false }
-      validPatterns.any { pattern ->
+      validPatterns.sumOf { pattern ->
         val desiredDesignSubstring = desiredDesign.drop(pattern.length)
-        isValidDesign(desiredDesignSubstring, availablePatterns)
+        countValidDesigns(desiredDesignSubstring, availablePatterns)
       }
     }
   }
@@ -29,9 +30,17 @@ fun main() {
   fun part1(): Int {
     val linen = readInput()
     return linen.desiredDesigns.count { desiredDesign ->
-      isValidDesign(desiredDesign, linen.availablePatterns)
+      countValidDesigns(desiredDesign, linen.availablePatterns) > 0
+    }
+  }
+
+  fun part2(): Long {
+    val linen = readInput()
+    return linen.desiredDesigns.sumOf { desiredDesign ->
+      countValidDesigns(desiredDesign, linen.availablePatterns)
     }
   }
 
   part1().println()
+  part2().println()
 }
